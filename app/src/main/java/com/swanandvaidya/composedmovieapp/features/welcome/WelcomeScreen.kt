775bacbe51d41f2528deeb1c0519df6eecacbb14
Swanand Vaidya <ssvaidya28@gmail.com>
2024-03-13 23:01:43 +0530

    removed unused imports

--- app/src/main/java/com/swanandvaidya/composedmovieapp/features/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/swanandvaidya/composedmovieapp/features/welcome/WelcomeScreen.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
